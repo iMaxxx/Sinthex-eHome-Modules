c77adaaf7b9a72e327532d06f7e0b2e0c7ba0177
iMaxxx <sinthernn@KT8612.local>
2016-08-11 14:17:14 +0200

    fix

--- RemkoSmartWeb/module.php
+++ RemkoSmartWeb/module.php
@@ -16,6 +16,7 @@
             // Diese Zeile nicht löschen.
             parent::Create();
 			$this->RegisterPropertyString("Address", "");
+			$this->RegisterVariables();
         }
 		
 		// Überschreibt die intere IPS_ApplyChanges($id) Funktion
@@ -45,93 +46,67 @@
 		}
 		
 		public function RegisterVariables() {
-			$varId = $this->RegisterVariableInteger("RSW_ID5033","Current operation mode");
-			IPS_SetVariableProfileValues($profileName, floatval($parameterDescriptor->MinValue), floatval($parameterDescriptor->MaxValue), floatval($parameterDescriptor->StepWidth));
-			IPS_SetVariableCustomProfile($this->GetIDForIdent("WSS_".$parameterDescriptor->ValueId), $profileName);
-			IPS_SetVariableProfileAssociation($profileName, $listItem->Value, $listItem->DisplayText, $this->TranslateIcon($listItem->ImageName), -1);
-			IPS_SetVariableProfileText($profileName,$parameterDescriptor->NamePrefix,$parameterDescriptor->Unit);
 			
-			$varId = $this->RegisterVariableFloat("RSW_ID5032","Outside Temperature");
-			SetValue($varId,floatval($values[1]));
+			// PROFILES
+			$profileName = "RSW_OperationMode";
+			IPS_CreateVariableProfile($profileName, 1);
+			IPS_SetVariableCustomProfile($varId, $profileName);
+			IPS_SetVariableProfileAssociation($profileName, 0, "", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 1, "Störung", "", 0xFF0000);
+			IPS_SetVariableProfileAssociation($profileName, 2, "HZG Puffer", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 3, "Abtaupuffer", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 4, "WW Puffer", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 5, "solar Heizen", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 6, "Heizen", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 7, "Kühlen", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 8, "Pool", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 9, "Umwälzung", "", -1);
+			PS_SetVariableProfileAssociation($profileName, 10, "Standby", "", -1);
 			
-			$varId = $this->RegisterVariableInteger("RSW_ID1079","Water operation mode");
-			SetValue($varId,intval($values[2]));
+			$profileName = "RSW_WaterOperationMode";
+			IPS_CreateVariableProfile($profileName, 1);
+			IPS_SetVariableCustomProfile($varId, $profileName);
+			IPS_SetVariableProfileAssociation($profileName, 0, "Automatic Komfort", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 1, "Automatik Eco", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 2, "nur Solar", "", -1);
 			
-			$varId = $this->RegisterVariableInteger("RSW_ID1088","Heating mode");
-			SetValue($varId,intval($values[3]));
+			$profileName = "RSW_HeatingMode";
+			IPS_CreateVariableProfile($profileName, 1);
+			IPS_SetVariableCustomProfile($varId, $profileName);
+			IPS_SetVariableProfileAssociation($profileName, 1, "Automatik", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 2, "Heizen", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 3, "Standby", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 4, "Kühlen", "", -1);
 			
-			$varId = $this->RegisterVariableBoolean("RSW_ID1992","One time water heating");
-			SetValue($varId,intval($values[4])); 
+			$profileName = "RSW_ActiveInactive";
+			IPS_CreateVariableProfile($profileName, 1);
+			IPS_SetVariableCustomProfile($varId, $profileName);
+			IPS_SetVariableProfileAssociation($profileName, 0, "Deaktiviert", "", -1);
+			IPS_SetVariableProfileAssociation($profileName, 1, "Aktiviert", "", -1);
 			
-			$varId = $this->RegisterVariableBoolean("RSW_ID1894","Party mode");
-			SetValue($varId,boolval($values[5]));
-			
-			$varId = $this->RegisterVariableBoolean("RSW_ID1893","Absent mode");
-			SetValue($varId,boolval($values[6]));
-			
-			$varId = $this->RegisterVariableBoolean("RSW_ID1022","Cooling functionality");
-			SetValue($varId,boolval($values[7]));
+			$this->RegisterVariableInteger("RSW_ID5033","Current operation mode","RSW_OperationMode");
+			$this->RegisterVariableFloat("RSW_ID5032","Outside Temperature","~Temperature");
+			$this->RegisterVariableInteger("RSW_ID1079","Water operation mode","RSW_WaterOperationMode");
+			$this->RegisterVariableInteger("RSW_ID1088","Heating mode","RSW_HeatingMode");				
+			$this->RegisterVariableBoolean("RSW_ID1992","One time water heating","RSW_ActiveInactive");
+			$this->RegisterVariableBoolean("RSW_ID1894","Party mode","RSW_ActiveInactive");
+			$this->RegisterVariableBoolean("RSW_ID1893","Absent mode","RSW_ActiveInactive");
+			$this->RegisterVariableBoolean("RSW_ID1022","Cooling functionality","RSW_ActiveInactive");
 		}
 		
 		public function GetValues() {
 			$data = $this->GetData("index.cgi?read");
 			if (isset($data)) {
 				$values = explode(",",$data);
-				IPS_LogMessage("RSW","ANTWORT:      ".$data);
-				$varId = $this->RegisterVariableInteger("RSW_ID5033","Current operation mode");
-				$profileName = "RSW_OperationMode";
-				IPS_CreateVariableProfile($profileName, 1);
-				IPS_SetVariableCustomProfile($varId, $profileName);
-				IPS_SetVariableProfileAssociation($profileName, 0, "", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 1, "Störung", "", 0xFF0000);
-				IPS_SetVariableProfileAssociation($profileName, 2, "HZG Puffer", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 3, "Abtaupuffer", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 4, "WW Puffer", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 5, "solar Heizen", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 6, "Heizen", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 7, "Kühlen", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 8, "Pool", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 9, "Umwälzung", "", -1);
-				PS_SetVariableProfileAssociation($profileName, 10, "Standby", "", -1);
+				SetValue($this->GetIDForIdent('RSW_ID5033'),$data[0]);
+				SetValue($this->GetIDForIdent('RSW_ID5032'),$data[1]);
+				SetValue($this->GetIDForIdent('RSW_ID1079'),$data[2]);
+				SetValue($this->GetIDForIdent('RSW_ID1088'),$data[3]);
+				SetValue($this->GetIDForIdent('RSW_ID1992'),$data[4]);
+				SetValue($this->GetIDForIdent('RSW_ID1894'),$data[5]);
+				SetValue($this->GetIDForIdent('RSW_ID1893'),$data[6]);
+				SetValue($this->GetIDForIdent('RSW_ID1022'),$data[7]);
 				
-				
-				$varId = $this->RegisterVariableFloat("RSW_ID5032","Outside Temperature","~Temperature");
-				
-				
-				$varId = $this->RegisterVariableInteger("RSW_ID1079","Water operation mode");
-				$profileName = "RSW_WaterOperationMode";
-				IPS_CreateVariableProfile($profileName, 1);
-				IPS_SetVariableCustomProfile($varId, $profileName);
-				IPS_SetVariableProfileAssociation($profileName, 0, "Automatic Komfort", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 1, "Automatik Eco", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 2, "nur Solar", "", -1);
-				
-				$varId = $this->RegisterVariableInteger("RSW_ID1088","Heating mode");
-				$profileName = "RSW_HeatingMode";
-				IPS_CreateVariableProfile($profileName, 1);
-				IPS_SetVariableCustomProfile($varId, $profileName);
-				IPS_SetVariableProfileAssociation($profileName, 1, "Automatik", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 2, "Heizen", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 3, "Standby", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 4, "Kühlen", "", -1);
-				
-				
-				$varId = $this->RegisterVariableBoolean("RSW_ID1992","One time water heating");
-				$profileName = "RSW_OneTimeWaterMode";
-				IPS_CreateVariableProfile($profileName, 1);
-				IPS_SetVariableCustomProfile($varId, $profileName);
-				IPS_SetVariableProfileAssociation($profileName, 1, "Störung", "", -1);
-				
-				$varId = $this->RegisterVariableBoolean("RSW_ID1894","Party mode");
-				$profileName = "RSW_ActiveInactive";
-				IPS_CreateVariableProfile($profileName, 1);
-				IPS_SetVariableCustomProfile($varId, $profileName);
-				IPS_SetVariableProfileAssociation($profileName, 0, "Deaktiviert", "", -1);
-				IPS_SetVariableProfileAssociation($profileName, 1, "Aktiviert", "", -1);
-				
-				$varId = $this->RegisterVariableBoolean("RSW_ID1893","Absent mode",$profileName);
-
-				$varId = $this->RegisterVariableBoolean("RSW_ID1022","Cooling functionality",$profileName);
 			}
 		}
 	

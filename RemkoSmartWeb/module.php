a6dbf40599b7125009104399f4d61b242d941e78
iMaxxx <sinthernn@KT8612.local>
2016-08-11 11:24:39 +0200

    fix

--- RemkoSmartWeb/module.php
+++ RemkoSmartWeb/module.php
@@ -35,7 +35,7 @@
 			curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36');
 			$this->SetStatus(102);
 			$page = curl_exec($curl);
-			return ($data);
+			return ($page);
 		}
 		
 		public function GetValues() {

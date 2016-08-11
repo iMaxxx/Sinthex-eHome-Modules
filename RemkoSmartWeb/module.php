6d3f862af3201d94f665e1c7718d47eb5e6cd733
iMaxxx <sinthernn@KT8612.local>
2016-08-11 14:02:54 +0200

    update profiels

--- RemkoSmartWeb/module.php
+++ RemkoSmartWeb/module.php
@@ -35,7 +35,7 @@
 			curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36');
 			$this->SetStatus(102);
 			$page = curl_exec($curl);
-			if (curl_getinfo($curl, CURLINFO_HTTP_CODE) <= 300 || curl_getinfo($curl, CURLINFO_HTTP_CODE) == 304) {
+			if (curl_getinfo($curl, CURLINFO_HTTP_CODE) <= 300) {
 				$this->SetStatus(102);	
 				return ($page);
 			} else {
@@ -44,85 +44,95 @@
 			}
 		}
 		
+		public function RegisterVariables() {
+			$varId = $this->RegisterVariableInteger("RSW_ID5033","Current operation mode");
+			IPS_SetVariableProfileValues($profileName, floatval($parameterDescriptor->MinValue), floatval($parameterDescriptor->MaxValue), floatval($parameterDescriptor->StepWidth));
+			IPS_SetVariableCustomProfile($this->GetIDForIdent("WSS_".$parameterDescriptor->ValueId), $profileName);
+			IPS_SetVariableProfileAssociation($profileName, $listItem->Value, $listItem->DisplayText, $this->TranslateIcon($listItem->ImageName), -1);
+			IPS_SetVariableProfileText($profileName,$parameterDescriptor->NamePrefix,$parameterDescriptor->Unit);
+			
+			$varId = $this->RegisterVariableFloat("RSW_ID5032","Outside Temperature");
+			SetValue($varId,floatval($values[1]));
+			
+			$varId = $this->RegisterVariableInteger("RSW_ID1079","Water operation mode");
+			SetValue($varId,intval($values[2]));
+			
+			$varId = $this->RegisterVariableInteger("RSW_ID1088","Heating mode");
+			SetValue($varId,intval($values[3]));
+			
+			$varId = $this->RegisterVariableBoolean("RSW_ID1992","One time water heating");
+			SetValue($varId,intval($values[4])); 
+			
+			$varId = $this->RegisterVariableBoolean("RSW_ID1894","Party mode");
+			SetValue($varId,boolval($values[5]));
+			
+			$varId = $this->RegisterVariableBoolean("RSW_ID1893","Absent mode");
+			SetValue($varId,boolval($values[6]));
+			
+			$varId = $this->RegisterVariableBoolean("RSW_ID1022","Cooling functionality");
+			SetValue($varId,boolval($values[7]));
+		}
+		
 		public function GetValues() {
 			$data = $this->GetData("index.cgi?read");
 			if (isset($data)) {
 				$values = explode(",",$data);
 				IPS_LogMessage("RSW","ANTWORT:      ".$data);
-				$varId = $this->RegisterVariableInteger("RSW_ID5033","current operation mode");
-				SetValue($varId,intval($values[0]));
+				$varId = $this->RegisterVariableInteger("RSW_ID5033","Current operation mode");
+				$profileName = "RSW_OperationMode";
+				IPS_CreateVariableProfile($profileName, 1);
+				IPS_SetVariableCustomProfile($varId, $profileName);
+				IPS_SetVariableProfileAssociation($profileName, 0, "", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 1, "Störung", "", 0xFF0000);
+				IPS_SetVariableProfileAssociation($profileName, 2, "HZG Puffer", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 3, "Abtaupuffer", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 4, "WW Puffer", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 5, "solar Heizen", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 6, "Heizen", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 7, "Kühlen", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 8, "Pool", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 9, "Umwälzung", "", -1);
+				PS_SetVariableProfileAssociation($profileName, 10, "Standby", "", -1);
 				
-				$varId = $this->RegisterVariableFloat("RSW_ID5032","Outside Temperature");
-				SetValue($varId,floatval($values[1]));
+				
+				$varId = $this->RegisterVariableFloat("RSW_ID5032","Outside Temperature","~Temperature");
+				
 				
 				$varId = $this->RegisterVariableInteger("RSW_ID1079","Water operation mode");
-				SetValue($varId,intval($values[2]));
+				$profileName = "RSW_WaterOperationMode";
+				IPS_CreateVariableProfile($profileName, 1);
+				IPS_SetVariableCustomProfile($varId, $profileName);
+				IPS_SetVariableProfileAssociation($profileName, 0, "Automatic Komfort", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 1, "Automatik Eco", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 2, "nur Solar", "", -1);
 				
 				$varId = $this->RegisterVariableInteger("RSW_ID1088","Heating mode");
-				SetValue($varId,intval($values[3]));
+				$profileName = "RSW_HeatingMode";
+				IPS_CreateVariableProfile($profileName, 1);
+				IPS_SetVariableCustomProfile($varId, $profileName);
+				IPS_SetVariableProfileAssociation($profileName, 1, "Automatik", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 2, "Heizen", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 3, "Standby", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 4, "Kühlen", "", -1);
+				
 				
 				$varId = $this->RegisterVariableBoolean("RSW_ID1992","One time water heating");
-				SetValue($varId,intval($values[4])); 
+				$profileName = "RSW_OneTimeWaterMode";
+				IPS_CreateVariableProfile($profileName, 1);
+				IPS_SetVariableCustomProfile($varId, $profileName);
+				IPS_SetVariableProfileAssociation($profileName, 1, "Störung", "", -1);
 				
 				$varId = $this->RegisterVariableBoolean("RSW_ID1894","Party mode");
-				SetValue($varId,boolval($values[5]));
+				$profileName = "RSW_ActiveInactive";
+				IPS_CreateVariableProfile($profileName, 1);
+				IPS_SetVariableCustomProfile($varId, $profileName);
+				IPS_SetVariableProfileAssociation($profileName, 0, "Deaktiviert", "", -1);
+				IPS_SetVariableProfileAssociation($profileName, 1, "Aktiviert", "", -1);
 				
-				$varId = $this->RegisterVariableBoolean("RSW_ID1893","Absent mode");
-				SetValue($varId,boolval($values[6]));
-				
-				$varId = $this->RegisterVariableBoolean("RSW_ID1022","Cooling functionality");
-				SetValue($varId,boolval($values[7]));
-			}
-			/*
-			$value[]
-			var arr=res.split(",");
-				if(arr.length!=9)return; //the last string is empty
-				
-				// arr[0] - ID5033 actual operation mode
-				if(arr[0] == 0)document.getElementById("s1item2name").innerHTML="";
-				else if(arr[0] == 1)document.getElementById("s1item2name").innerHTML="StÃ¶rung"; 
-				else if(arr[0] == 2)document.getElementById("s1item2name").innerHTML="HZG Puffer"; 
-				else if(arr[0] == 3)document.getElementById("s1item2name").innerHTML="Abtaupuffer"; 
-				else if(arr[0] == 4)document.getElementById("s1item2name").innerHTML="WW Puffer"; 
-				else if(arr[0] == 5)document.getElementById("s1item2name").innerHTML="solar Heizen"; 
-				else if(arr[0] == 6)document.getElementById("s1item2name").innerHTML="Heizen"; 
-				else if(arr[0] == 7)document.getElementById("s1item2name").innerHTML="KÃ¼hlen"; 
-				else if(arr[0] == 8)document.getElementById("s1item2name").innerHTML="Pool"; 
-				else if(arr[0] == 9)document.getElementById("s1item2name").innerHTML="UmwÃ¤lzung"; 
-				else document.getElementById("s1item2name").innerHTML="Standby";
-				
-				// arr[1] - ID5032 outside temperature E0041
-				document.getElementById("s1item4name").innerHTML=arr[1]+" Â°C";
-				
-				// arr[2] - ID1079 operating mode DHW
-				if(arr[2] == 0)document.getElementById("s1item6name").innerHTML="Automatik Komfort";
-				else if(arr[2] == 1)document.getElementById("s1item6name").innerHTML="Automatik Eco";
-				else document.getElementById("s1item6name").innerHTML="nur Solar";
-				
-				// arr[3] - ID1951/ID1088 heating mode (cooling disabled/enabled)
-				if(arr[3] == 1)document.getElementById("s1item8name").innerHTML="Automatik";
-				else if(arr[3] == 2)document.getElementById("s1item8name").innerHTML="Heizen";
-				else if(arr[3] == 3)document.getElementById("s1item8name").innerHTML="Standby";
-				else document.getElementById("s1item8name").innerHTML="KÃ¼hlen";
-				
-				// arr[4] - ID1992 1 x DHW heating
-				if(arr[4] == 0)document.getElementById("s1item10name").innerHTML="deaktiviert";
-				else document.getElementById("s1item10name").innerHTML="aktiviert";
+				$varId = $this->RegisterVariableBoolean("RSW_ID1893","Absent mode",$profileName);
 
-				// arr[5] - ID1894 party mode
-				if(arr[5] == 0)document.getElementById("s1item12name").innerHTML="deaktiviert";
-				else document.getElementById("s1item12name").innerHTML="aktiviert";
-				
-				// arr[6] - ID1893 absent mode
-				if(arr[6] == 0)document.getElementById("s1item14name").innerHTML="deaktiviert";
-				else document.getElementById("s1item14name").innerHTML="aktiviert";
-				
-				// arr[7] - ID1022 cooling functionality (disable / enable)
-				if(arr[7] == 0)document.getElementById("s1item7url").href="popup_roomclimate_0.html";
-				else document.getElementById("s1item7url").href="popup_roomclimate_1.html";
-			 * 
-			 * 
-			 */
+				$varId = $this->RegisterVariableBoolean("RSW_ID1022","Cooling functionality",$profileName);
+			}
 		}
 	
 	public function SetValue($valueId, $value) {

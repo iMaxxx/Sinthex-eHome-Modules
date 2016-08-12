fcbcd16dba07a5e91c93772322d82300fda6fd95
iMaxxx <sinthernn@KT8612.local>
2016-08-12 10:42:26 +0200

    test

--- RemkoSmartWeb/module.php
+++ RemkoSmartWeb/module.php
@@ -197,13 +197,13 @@
 	
 	public function RequestAction($ident, $value) {
 		IPS_LogMessage("RSW","IDENT:   $ident       VALUE:   $value");
-	    $this->WriteValue($ident,$value);
+	    $this->WriteValue($ident, $value);
 		SetValue($this->GetIDForIdent($ident), $value);
 	  }
 	
 	public function WriteValue($ident, $value) {
 		$data = $this->GetData("index.cgi?index.cgi?".$ident."=".$value);
-		IPS_LogMessage("RSW","IDENT:   $ident       VALUE:   $value");
+		IPS_LogMessage("RSW","index.cgi?index.cgi?".$ident."=".$value);
 		//$this->GetValues();
 	}
 }

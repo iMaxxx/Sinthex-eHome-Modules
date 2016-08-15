50ebb1966aac988cb41473b05db417b5f4065200
iMaxxx <sinthernn@KT8612.local>
2016-08-15 09:03:28 +0200

    fix

--- RemkoSmartWeb/module.php
+++ RemkoSmartWeb/module.php
@@ -150,7 +150,7 @@
 				IPS_SetName($eid, $eventName); 
 				IPS_SetParent($eid, $this->InstanceID);         //Eregnis zuordnen
 				IPS_SetEventActive($eid, true);             //Ereignis aktivieren
-				$script = 'RSW_GetValues($id);';
+				$script = '$id = \$_IPS["TARGET"];RSW_GetValues($id);';
 				//IPS_SetEventScript($eid, "\$id = \$_IPS['TARGET'];\n$script;");
 				IPS_SetEventScript($eid, $script);
 			}

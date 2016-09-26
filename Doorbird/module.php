9ed9883c26529cc6108b256074defb2b9d0344a2
iMaxxx <sinthernn@KT8612.local>
2016-09-26 15:22:46 +0200

    fix

--- Doorbird/module.php
+++ Doorbird/module.php
@@ -44,7 +44,7 @@
 			//curl_setopt($curl, CURLOPT_HTTPHEADER,$header);
 			curl_setopt($curl, CURLOPT_CUSTOMREQUEST,"GET");
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+			//curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 			curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
 			curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36');
 			$this->SetStatus(102);

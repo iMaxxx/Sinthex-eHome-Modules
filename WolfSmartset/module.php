2a2b73a7230f4d7f69384ba32ba812e0f89f8447
iMaxxx <sinthernn@KT8612.local>
2017-01-08 14:33:35 +0100

    fix

--- WolfSmartset/module.php
+++ WolfSmartset/module.php
@@ -280,6 +280,7 @@
 					foreach($list->ParameterDescriptors as &$parameterDescriptor) {
 						$this->LogDebug('$parameterDescriptor', print_r($parameterDescriptor,true));
 						$this->LogDebug('$tabGuiId',$tabGuiId);
+						$this->LogDebug('$parameterDescriptor->ValueId',$parameterDescriptor->ValueId);
 						$this->LogDebug('$properties[$tabGuiId]["ID".$parameterDescriptor->ParameterId]',print_r($properties[$tabGuiId]["ID".$parameterDescriptor->ParameterId],true));
 						$properties[$tabGuiId]["ID".$parameterDescriptor->ParameterId]->ValueId=$parameterDescriptor->ValueId;
 						$this->LogDebug('$parameterDescriptor->ParameterId->ValueId', $parameterDescriptor->ValueId);
